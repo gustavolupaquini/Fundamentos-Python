102a3fdb88564a4cbc5026c1d32d8ffdc8103f6d
Gustavo Lupaquini <gustavo_spinellilupaquini@outlook.com>
2024-08-31 11:56:55 -0300

    Aprendendo Python

---
+++ basic-python.py
@@ -0,0 +1,66 @@
+#Print
+
+print ("Olá, Mundo!")
+
+#Variaveis Inteiros
+#Podemos atribuir o mesmo valor para varias variaveis na mesma linha
+
+n1 = 10
+n2 = 5
+a = b = c = 10
+
+somai = (n1 + n2)
+
+#Variveis Float
+
+n3 = 9.01
+n4 = 9.19
+somaf = (n3 + n4)
+
+#Strings
+#Letras maiusculas diferenciam variaveis
+
+nome = "Gustavo"
+Nome = "Gustavo"
+sobrenome = 'Lupaquini'
+
+#Boolean
+
+isMaiordeIdade = True
+temDesconto = False
+
+#Aritméticos
+
+a = 10
+b = 3
+
+soma = a + b   # 13
+subtracao = a - b    # 7
+multiplicacao = a * b    # 30
+divisao = a / b   # 3.333333333
+divisao_inteira = a // b   # 3
+modulo = a % b   # 1
+exponenciacao = a ** b   # 1000
+
+#Operadores de comparacao
+
+a = 10
+b = 3
+
+igual = a == b   # False
+diferente = a != b   # True
+maior_que = a > b   # True
+menor_que = a < b   # False
+maior_ou_igual = a >= b   # True
+menor_ou_igual = a <= b   # False
+
+#Lógicos
+#NOT inverte, ou seja, se é true ele retorna false
+
+a = 10
+b = 3
+
+resultado_and = (a > 5) and (b < 5)   # True
+resultado_or = (a > 15) or (b < 5)   # True
+resultado_not = not (a > 5)   # False
+
---
+++ est-condicionais.py
@@ -0,0 +1,26 @@
+#IF / ELSE
+
+idade = 18
+
+if idade >= 18:
+   print ("Você é maior de idade.")
+else:
+   print ("Você é menor de idade.")
+
+#IF / ELIF / ELSE
+#Quando uma avaliacao for positiva, a condicao ja retorna e nao avalia as proximas.
+
+nota = 85
+
+if nota >= 90:
+   print ("Excelente")
+
+elif nota >= 80:
+   print ("Muito bom")
+
+elif nota >= 70:
+   print ("Bom")
+
+else:
+   print ("Precisa melhorar")
+
